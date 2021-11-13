d850f201826711f76867df99c1478d55043d366a
agusminahk <agusminahk99@gmail.com>
2021-11-13 09:13:14 -0300

    configs & folders

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Cover from './components/Cover/Cover';
+import './App.css';
+
+function App() {
+    return (
+        <div>
+            <Cover />
+        </div>
+    );
+}
+
+export default App;
